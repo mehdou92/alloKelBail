e4ac619d30461172f002154a1d89c6d24d92692f
Mehdi <mehdiab92@gmail.com>
2019-07-01 15:53:47 +0200

    adding custom hooks form for sign in

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import Navbar from '../Navbar';
+import SignIn from '../SignIn';
 import { TodoContext } from '../../store/TodoContext';
 
 function App() {
   return (
     <div className="App">
-      <TodoContext.Provider
+      {/* <TodoContext.Provider
         value={{
           todos: [
             {text: "test context"}
           ]
         }}
 
-        />
-      <Navbar />
+        /> */}
+
+
+
+      {/* <Navbar /> */}
+      <SignIn />
     </div>
   );
 }

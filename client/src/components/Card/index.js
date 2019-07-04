b269efff8ff6222a68ac394f2434df36e86d43a9
Mehdi <mehdiab92@gmail.com>
2019-07-04 18:26:34 +0200

    add button when user is connected

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -8,6 +8,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '../IconButtons';
+import { AuthContext } from '../Auth/AuthProvider';
 
 const useStyles = makeStyles({
   card: {
@@ -17,6 +18,14 @@ const useStyles = makeStyles({
 
 export default function ImgMediaCard(props) {
   const classes = useStyles();
+  const { user } = useContext(AuthContext);
+
+  const displayButton = () => {
+    if (user) {
+      const tmp = [<IconButton icon="favorite_border" label="I like it !" color="secondary" />, <IconButton icon="star_border" label="Movie seen" color="primary" />];
+      return tmp;
+    }
+  }
 
   return (
     <Card className={classes.card}>
@@ -41,8 +50,7 @@ export default function ImgMediaCard(props) {
         <Button size="small" color="primary">
           See more
         </Button>
-        <IconButton icon="favorite_border" label="I like it !" color="secondary"/>
-        <IconButton icon="star_border" label="Movie seen" color="primary"/>
+        {displayButton()}
       </CardActions>
     </Card>
   );
